92eb6a728f6cb5f009f19d1387a7f48e6742a557
lindolo25 <lindolo25@outlook.com>
2018-11-28 20:57:39 -0500

    completed the functionallity for the survey POST action and the chanlges jon the UI to shot the best match.

--- app/public/assets/main.js
+++ app/public/assets/main.js
@@ -1,12 +1,13 @@
 let init = function()
 {
     $("#survey").on("submit", surveySubmitHandler);
-    $('.survey-question input').change(questionChange);
+    $('#survey-questions').on("change", ".survey-question input", questionChange);
+    loadQuestions();
 }
 
 let surveySubmitHandler = function(event)
 {
-    console.log("form submited");
+    //console.log("form submited");
     event.preventDefault();
     var surveyResult = {
         name: $("#survey-name").val().trim(),
@@ -36,12 +37,53 @@ let questionChange = function ()
 {
     var value = $(this).attr("data-value");
     $(this).parents(".survey-question").attr("data-value", value);
-    console.log(value);
+    //console.log(value);
 }
 
 let sendSurvey = function(surveyResult)
 {
-    console.log(surveyResult);
+    $.ajax({ 
+        url: "/api/survey", 
+        method: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(surveyResult)
+    }).then(function(match) 
+        {
+            showMatch(match);
+        });
+}
+
+let showMatch = function(match)
+{
+    var image = $('<img class="profile-photo" src="{0}" />'.format(match.photo));
+    var name = $('<h1 class="profile-name">{0}</h1>'.format(match.name));
+    var link = $('<a href="./" class="btn btn-primary">Go Back Home</a>');
+
+    $("#document-body").empty().append(image, name, link).addClass("text-center");
+}
+
+let loadQuestions = function()
+{
+    $.ajax({ url: "/api/survey", method: "GET" })
+        .then(function(questions) 
+        {
+            for (var i = 0; i < questions.length; i++) 
+            {
+                let index = $('<div class="col-1"><h2>{0}.</h2></div>'.format(1 + i));
+                let qBody = $('<div class="col-11"><p>{0}</p></div>'.format(questions[i]));
+                let options = $('<div class="col-11 offset-1"></div>');
+                for(y = 1; y < 6; y++)
+                {
+                    options.append('<div class="custom-control custom-radio form-check form-check-inline">\
+                            <input type="radio" class="custom-control-input" id="survey-option-{0}{1}" data-value="{1}" name="survey-value-{0}" required>\
+                            <label class="custom-control-label" for="survey-option-{0}{1}">{1}</label>\
+                        </div>'.format(i, y));
+                }
+                let question = $('<section id="survey-question-{0}" class="survey-question row" data-index="{0}" data-value="0"></section>'.format(i));
+                question.append(index, qBody, options)
+                $("#survey-questions").append(question);
+            }
+        });
 }
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,6 +15,36 @@ module.exports = function(app)
 
     app.post("/api/survey", function(req, res) 
     {
-        res.json(true);
+        var user = req.body;
+        match = {
+            data: 0,
+            diff: user.scores.length * 5
+        }
+
+        for(i = 0; i < friends.length; i++)
+        {
+            var current =  friends[i].scores;
+            var currentDiff = 0;
+            //console.log(i);
+            for(y = 0; y < current.length; y++)
+            {
+                currentDiff += Math.abs(current[y] - user.scores[y]);
+            }
+
+            if(currentDiff === 0)
+            {
+                match.data = friends[i];
+                match.diff = currentDiff;
+                break;
+            }
+            else if(currentDiff < match.diff)
+            {
+                match.data = friends[i];
+                match.diff = currentDiff;
+            }
+        }
+
+        friends.push(user);
+        res.json(match.data);
     });
 };
